d97ce9622500d29965c30d74dc563e8589ca9884
Sameer Kumar Kalyanshetti <95131876+SameerShetty@users.noreply.github.com>
2022-07-02 14:09:33 +0530

    fourth

--- index.js
+++ index.js
@@ -22,3 +22,47 @@ navbtn.onclick = function () {
     b3.style.transform = "rotate(0)translateY(0)";
   }
 };
+// preloader
+var loader = document.getElementById("preloader");
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+});
+
+//nav-active
+const nav = document.querySelector(".nav");
+window.addEventListener("scroll", fixnav);
+var link = document.querySelectorAll(".nav-link");
+var i;
+function fixnav() {
+  if (window.scrollY > nav.offsetHeight + 40) {
+    nav.classList.add("activenav");
+    for (i = 0; i < 7; i++) {
+      link[i].style.color = "#f6f6f6";
+    }
+  } else {
+    nav.classList.remove("activenav");
+    for (i = 0; i < 7; i++) {
+      link[i].style.color = "#34374c";
+    }
+  }
+}
+
+//html element reveal
+
+window.addEventListener("scroll", reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
